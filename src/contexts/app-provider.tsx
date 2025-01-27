30a5045de7b97f923199c6943b71aeb3d0acb96a
LauWyeRock <e0726426@u.nus.edu>
2025-01-27 17:46:40 +0800

    JWT Frontend changes

--- src/contexts/app-provider.tsx
+++ src/contexts/app-provider.tsx
@@ -7,8 +7,9 @@ import store, {
   setUser,
   setUserProfile,
 } from '@/store/store';
-import { deleteToken, storeToken } from '@/utils/token';
+import { deleteToken, exchangeFirebaseToken, storeToken, getToken } from '@/utils/token';
 import { onAuthStateChanged, User } from 'firebase/auth';
+import { useRouter, usePathname } from 'next/navigation';
 import { ReactNode, useEffect } from 'react';
 import { Provider, useDispatch } from 'react-redux';
 
@@ -26,27 +27,43 @@ const AppProvider = ({ children }: Props) => {
 };
 
 const AppProviderContent = ({ children }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
   const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       dispatch(resetState());
       if (user) {
-        const token = await user.getIdToken();
-        storeToken(token);
-        const userProfile = await fetchUserProfile();
+        const existingToken = getToken();
+        if (!existingToken) {
 
+          const token = await user.getIdToken();
+          const { access } = await exchangeFirebaseToken(token);
+          storeToken(access);
+
+        }
+
+        const userProfile = await fetchUserProfile();
         dispatch(setUser(user.toJSON() as User));
         dispatch(setUserProfile(userProfile));
+        
+        if (pathname === '/login') {
+          router.push('/');
+        }
       } else {
         deleteToken();
         dispatch(setUser(null));
         dispatch(setUserProfile(null));
+
+        if ( pathname !== '/login' && pathname !== '/signup') {
+          router.push('/login');
+        }
       }
     });
 
     return unsubscribe;
-  }, [dispatch]);
+  }, [dispatch, router, pathname]);
 
   return <>{children}</>;
 };
